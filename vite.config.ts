8617fa940faa0aa28af1f405b66056990a6ab9c3
jarano2022 <jarano2023@gmail.com>
2025-11-08 12:40:40 +0100

    he añadido el host de render al archivo de configuración

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,8 @@ export default defineConfig(({ mode }) => {
     server: {
       host,
       port,
-      open: true
+      open: true,
+      allowedHosts: ['react-app-yg5h.onrender.com']
     }
   }
 })
